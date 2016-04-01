77fd8d8ecef87972b829f3db55dc91d461e82ded
Skyler <skyler.m.degrote@outlook.com>
2016-04-01 16:56:55 -0500

    character functionality still not working

--- code/client/js/characters.js
+++ code/client/js/characters.js
@@ -8,31 +8,3 @@ $("#mainCharacter").click(function(){
     $(".mainCharacter").toggle().removeClass("hidden").addClass("visible");
     $(this).addClass("active");
 });
-
-$("#majorCharacter").click(function(){
-    $(".active").removeClass("active");
-    $(".visible").toggle().removeClass("visible").addClass("hidden");
-    $(".majorCharacter").toggle().removeClass("hidden").addClass("visible");
-    $(this).addClass("active");
-});
-
-$("#minorCharacter").click(function(){
-    $(".active").removeClass("active");
-    $(".visible").toggle().removeClass("visible").addClass("hidden");
-    $(".minorCharacter").toggle().removeClass("hidden").addClass("visible");
-    $(this).addClass("active");
-});
-
-$("#sideCharacter").click(function(){
-    $(".active").removeClass("active");
-    $(".visible").toggle().removeClass("visible").addClass("hidden");
-    $(".sideCharacter").toggle().removeClass("hidden").addClass("visible");
-    $(this).addClass("active");
-});
-
-$("#extraCharacter").click(function(){
-    $(".active").removeClass("active");
-    $(".visible").toggle().removeClass("visible").addClass("hidden");
-    $(".extraCharacter").toggle().removeClass("hidden").addClass("visible");
-    $(this).addClass("active");
-});
